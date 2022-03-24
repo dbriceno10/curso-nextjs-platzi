67d0773cb7ea052ae45ea80a6b12825d575f7ddd
Daniel Briceño <daniel.b100726@gmail.com>
2022-03-23 23:09:39 -0400

    Clase 4: Rutas básicas

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>This is the home page</p>
+    </div>
+  );
+};
+
+export default Home;
+
+//Como esta es una página básica, sin decirle donde la va a renderizar, como esta se llama index.js, la va a renderizar como ruta inicial.
