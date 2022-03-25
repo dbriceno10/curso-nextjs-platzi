f77f0d43c6ed7db974d36e297bec323c5e072ee7
Daniel Briceño <daniel.b100726@gmail.com>
2022-03-25 16:25:39 -0400

    Clase 8: Enlazando páginas

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Navbar from "../components/Navbar/Navbar";
 
 const Home = () => {
   return (
     <div>
+      <Navbar />
       <h1>Home</h1>
       <p>This is the home page</p>
     </div>
