2355313e72615e4b4dbb9829465a13491c26a1cf
Daniel Briceño <daniel.b100726@gmail.com>
2022-03-27 19:54:34 -0400

    Clase 14: Path alias

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "../../components/Navbar/Navbar";
+import Navbar from "@components/Navbar/Navbar";
 
 const Layout: React.FC = ({ children }) => {
   //React.FC ----> React Function Component <--- es el tipo de componente que será
